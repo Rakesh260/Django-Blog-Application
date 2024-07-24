fd8af5aa437505113bcdacb7bcbb4ae0d0967743
Rakesh260 <109200192+Rakesh260@users.noreply.github.com>
2024-07-24 12:32:40 +0530

    blog application base created

---
+++ django_blog/blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published_date = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_posts')
+
+    class Meta:
+        db_table = 'post'
+        managed = True
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'comments'
+        managed = True
